91dc619bbce60b36c146f04422adf8bb8554aeb6
Nicolas Leiva <nicolasleiva@gmail.com>
2020-06-12 10:10:12 -0400

    Update test file

--- tests/test.sh
+++ tests/test.sh
@@ -3,14 +3,18 @@
 VERSION=0.1.1
 TEST=02
 
+# Test locally
 rm -rf collections
 ansible-galaxy collection build --force
 ansible-galaxy collection install nleiva-capirca_acl-${VERSION}.tar.gz -p ./collections
 
 ansible-playbook test${TEST}.yml -vvv
 
-cd collections/ansible_collections/nleiva/capirca_acl
+# Test Collection
+mkdir ../temp
+ansible-galaxy collection install nleiva-capirca_acl-${VERSION}.tar.gz -p ../temp
+cd ../temp/ansible_collections/nleiva/capirca_acl
 # https://github.com/ansible/ansible/pull/69341
-# ansible-test integration --docker fedora29
 ansible-test integration --list-targets
+ansible-test integration --docker ubuntu1804
 cd ../../../..
